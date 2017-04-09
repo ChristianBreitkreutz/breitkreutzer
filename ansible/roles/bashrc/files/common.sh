071f919ff2ff6802058e5bb378407750cfb486c6
Christian Breitkreutz <chrisbea@gmx.net>
2017-04-09 20:45:31 +0200

    update general task for new approach

--- ansible/roles/bashrc/files/common.sh
+++ ansible/roles/bashrc/files/common.sh
@@ -11,23 +11,14 @@ alias l.='ls -d .* --color=auto'
 # cd
 alias cd..='cd ..'
 alias ..='cd ..'
-alias .2='cd ../..'
-alias .r='cd /root'
-alias .h='cd /home'
-if [ -d "/home/$USER/Schreibtisch" ]; then
-	alias .d="cd /home/$USER/Schreibtisch"
-else
-	alias .d="cd /home/$USER/Desktop"
-fi
-
 
 # rests
 alias tailf='tail -f -n50'
-alias c='clear'
 alias ping='ping -c 3'
 alias pingg='ping -c 1 www.google.com'
 alias df='df -H'
 alias ?=pwd
 
-EDITOR=vim
+alias catssh='cat ~/.ssh/*.pub'
 
+EDITOR=vim
