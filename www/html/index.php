6cfbe1172a81ee57b57bb031bf3427131b192da3
Christian Breitkreutz <chrisbea@gmx.net>
2017-07-11 23:59:53 +0200

    better start page

--- www/html/index.php
+++ www/html/index.php
@@ -1,12 +1,34 @@
-<h1>Meine Beat Atomes</h1>
+<html>
+<head>
+<style>
+
+a.outerBox {
+  padding: inherit;
+  padding-left: 200px;
+  padding-right: 1000px;
+  background-color: #dedede;
+
+}
+div.outerBox {
+  padding: 40px;
+  margin: 12px;
+}
+</style>
+</head>
+<body>
+  <h1>breitkreutzer</h1>
+  <hr>
 <?php
 if ($handle = opendir ( 'projects' )) {
 	while ( false !== ($file = readdir ( $handle )) ) {
-		if ($file != "." && $file != ".." ) {
-			echo '<li><a href="projects/' . $file . '">' . $file . '</a></li>';
+		if ($file != "." && $file != "..") {
+			echo '<div class=outerBox ><a class=outerBox href="projects/' . $file . '"><span class = "linkbar">' . $file . '</span></a></div>';
 		}
 	}
 	closedir ( $handle );
 }
 
 ?>
+
+</body>
+</html>
