803b7a8f2d738a82e735a601c1ab6878a8b7dee7
Christian Breitkreutz <cbreitkreutz@epages.com>
2017-10-06 23:59:37 +0200

    schon besser

--- www/html/index.php
+++ www/html/index.php
@@ -5,31 +5,51 @@
 <link href='https://fonts.googleapis.com/css?family=Abel' rel='stylesheet'>
 <link rel="stylesheet" href="normal.css">
 </head>
-<body>
-  <div class="header">
-    <h1>- breitkreutzer -</h1>
+<body >
+ 
+<div class="container">
+  <div class="headLine">
+      <div class="headline-item">
+      Projekte
+      </div>
+      <div class="headline-item">
+      |
+      </div>
+      <div class="headline-item">
+      Musik
+      </div>
+      <div class="headline-item">
+      |
+      </div>
+      <div class="headline-item" >
+      Ich
+      </div>
   </div>
-
-  <div class="menu">
-    
+  <div class="base">
 <?php
 if ($handle = opendir ( 'projects' )) {
-	while ( false !== ($folder = readdir ( $handle )) ) {
-		if ($folder != "." && $folder != "..") {
-			echo '<div>';
-			echo '  <a class="outer" href="projects/'.$folder. '"  >';
-			echo '    <div class="inner">';
-            echo '      <img  src="projects/'.$folder.'/icon.svg" height="99%"/>';
-	        echo $folder ;
-	        echo '    </div>';
-            echo '  </a>';
-            echo '</div>';
-		}
-	}
-	closedir ( $handle );
+    while ( false !== ($folder = readdir ( $handle )) ) {
+        if ($folder != "." && $folder != "..") {
+?>
+    <a href="projects/<?php echo $folder ?>" target="newTab" >
+    <?php
+        $myfile = fopen("projects/".$folder."/css.part", "r");
+        echo fread($myfile,filesize("projects/".$folder."/css.part"));
+        fclose($myfile);
+    ?>
+        <span class="text-with-background">
+            <?php echo $folder ?>
+          </span>
+      </div>
+    </a>
+<?php
+        }
+    }
+    closedir ( $handle );
 }
-
 ?>
   </div>
+</div>
+
 </body>
 </html>
