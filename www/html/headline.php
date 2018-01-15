d172488b22e6033b0db3045fb3986ae88ae004f2
Christian Breitkreutz <cbreitkreutz@epages.com>
2018-01-15 13:56:35 +0100

    cleanup before new setup

--- www/html/headline.php
+++ www/html/headline.php
@@ -1,17 +1,17 @@
 <div class="headLine">
-  <div class="headline-item">
+  <span class="headline-item">
     <a class="big-text" href="project.php">Projekte</a>
-  </div>
-  <div class="headline-item">
+  </span>
+  <span class="headline-item">
   |
-  </div>
-  <div class="headline-item">
+  </span>
+  <span class="headline-item">
      <a class="big-text" href="musik.php">Musik</a>
-  </div>
-  <div class="headline-item">
+  </span>
+  <span class="headline-item">
   |
-  </div>
-  <div class="headline-item" >
+  </span>
+  <span class="headline-item" >
     <a class="big-text" href="ich.php">Ich</a>
-  </div>
+  </span>
 </div>
